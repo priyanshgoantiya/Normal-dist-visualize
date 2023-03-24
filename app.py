362487ca18fa7738e93bf2e285ca0f6593b64f45
Suman Kumar Suman <samp.suman@gmail.com>
2023-03-24 23:35:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ st.markdown("""
 """)
 
 # Add all your application here
-app.add_app("KDE", kde.app)
+
 app.add_app("Normal Distribution", normal.app)
+app.add_app("KDE", kde.app)
 # The main app
 app.run()
